9314179b381c3a8825ea40025e5b2f32184ffe90
isakk7 <74216113+isakk7@users.noreply.github.com>
2022-07-17 20:15:25 -0500

    feat: proyect has inicialize

---
+++ src/components/Main.js
@@ -0,0 +1,54 @@
+import { Facebook, Instagram, LinkedIn, MenuBook, Pinterest, SettingsPowerRounded, Twitter } from '@material-ui/icons'
+import React, { useState } from 'react'
+import '../styles/Main.scss'
+import me from '../assets/mee.png'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className='main__container'>
+
+
+                <div className="main__content">
+
+                    <div className="text">
+                           
+                            <h1>Isaac Adan Lopez</h1>
+                            <h5>Soy una persona que le gusta mucho la tecnologia, y me gustaria aprender muchas cosas una vez terminada la carrera.
+                                </h5>
+
+                            <div className="icons">
+                                <Twitter className='icon'/>
+                                <Instagram className='icon'/>
+                                <Facebook className='icon'/>
+                                <LinkedIn className='icon'/>
+                                <Pinterest className='icon'/>
+                            </div>
+
+
+                    <div className="buttons">
+                        <button>See My Work</button>
+                        <button>Hire Me</button>
+                    </div>
+
+                    </div>
+
+
+                </div>
+
+
+                <div className="main__img">
+                    <img src={me} alt="" />
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+    )
+}
+
+
+export default Main
---
+++ src/components/header.js
@@ -0,0 +1,48 @@
+import { Close, FiberManualRecordOutlined, MenuOutlined } from '@material-ui/icons'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Header.scss'
+
+const Header = () => {
+
+    const [active,setActive] = useState(false);
+
+
+    const showMenu = () => {
+            setActive(!active)
+    }
+
+    return (
+        <div className='header'>
+
+                    <div className="header__logo">
+                        <h1>Leedd</h1>
+                    </div>
+
+            <nav className={active ? 'navbar active' : 'navbar'}>
+
+                <ul>
+                    <div className="closed">
+                        <Close className='close' onClick={showMenu}/>
+                    </div>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/'>Ingles</Link>
+                    </li>
+                    <li>
+                        <Link to='/'>Español</Link>
+                    </li>
+                   
+                </ul>
+            </nav>
+
+                    <div className="changer">
+                        <MenuOutlined className='menu' onClick={showMenu}/>
+                    </div>
+                    </div>
+    )
+}
+
+export default Header
